83296ce2084915cb394a45f034246d2af33043b6
prodstalker <prod.stalker@gmail.com>
2022-08-13 23:07:03 +0200

    Add header banner Genève Ville Vivante on the homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\HeaderBanner;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,11 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $headerBanner = (new HeaderBanner())->setTitle('Genève Ville Vivante')
+            ->setImageUrl('https://geneve-ville-vivante.ch/img/banners/home.jpg');
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'headerBanner' => $headerBanner,
         ]);
     }
 }
