b62bff5c62c1be1922a988471c547ce2eb320f13
prodstalker <prod.stalker@gmail.com>
2022-08-14 20:00:22 +0200

    Remove empty line

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -7,7 +7,6 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-
 use Gedmo\Mapping\Annotation as Gedmo;
 
 #[ORM\Entity(repositoryClass: ProjectRepository::class)]
